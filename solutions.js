b32a71a6aef65f65ad73bab4a8ff8b429b85e460
Lucas <lucas2carlos@gmail.com>
2021-12-26 17:42:51 -0500

    verbal questions completed

---
+++ solutions.js
@@ -0,0 +1,4 @@
+//// Verbal Questions ///////
+//1A. parameter is the variable given in the paraenthesis while creating the function.  Arguments is the variable you write in the parenthesis to replace the parameter when you call the funciton.
+//2A. return gives a value for the computer to store, whereas console.log simply logs whatever you place inside it for testing purposes.
+
