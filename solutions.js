55b04fe8886e56d04b24a512383b436d8af0f2fc
Lucas <lucas2carlos@gmail.com>
2021-12-26 17:54:12 -0500

    palindrome answered

--- solutions.js
+++ solutions.js
@@ -2,3 +2,9 @@
 //1A. parameter is the variable given in the paraenthesis while creating the function.  Arguments is the variable you write in the parenthesis to replace the parameter when you call the funciton.
 //2A. return gives a value for the computer to store, whereas console.log simply logs whatever you place inside it for testing purposes.
 
+///////Palindrome/////////
+const checkPalindrome = (str) =>{
+    return str.toUpperCase() == str.split('').reverse().join('').toUpperCase()
+}
+console.log(checkPalindrome('Radar'))
+
