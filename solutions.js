f36b3e97ebe7c3aa7365cd364b1d2c9c3ecc756a
Lucas <lucas2carlos@gmail.com>
2021-12-26 19:05:21 -0500

    sum array completed

--- solutions.js
+++ solutions.js
@@ -8,3 +8,14 @@ const checkPalindrome = (str) =>{
 }
 console.log(checkPalindrome('Radar'))
 
+///////////Sum Array////////////
+const sumArray = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum
+}
+console.log(sumArray([1,2,3,4,5,6]))
+
+
