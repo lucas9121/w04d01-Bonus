28f2c4c3fd9b7902e5ea68ee1846d5cf9b92c8ed
Lucas <lucas2carlos@gmail.com>
2021-12-27 22:43:40 -0500

    rock paper scissors completed

--- solutions.js
+++ solutions.js
@@ -49,3 +49,89 @@ const printPrimes = (num) => {
 
 printPrimes(78)
 
+
+////////////////Rock Paper Scissors//////////////
+const choice = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+const randomMove = () => {
+    let randomizer = Math.floor(Math.random() * choice.length);
+    return choice[randomizer];
+}
+
+let computerMove = randomMove();
+let usersMove 
+
+const arguments = process.argv.slice(2);
+//https://www.youtube.com/watch?v=X39LSNNZW14
+//process.argv allows a comands to be typed on terminal to be passed as arguments.
+//proccess.argv is an array of commands and there are two commands already built into it (reson for .slice)
+const playerMove = () =>{
+    let rock = 'rock'
+    let paper = 'paper'
+    let scissors = 'scissors'
+    let lizard = 'lizard'
+    let spock = 'spock'
+    if(arguments == rock){
+        usersMove = choice[0];
+    } else if(arguments == paper){
+        usersMove = choice[1];
+    } else if(arguments == scissors){
+        usersMove = choice[2];
+    } else if(arguments == lizard){
+        usersMove = choice[3];
+    } else if(arguments == spock){
+        usersMove = choice[4];
+    }
+}
+
+playerMove()
+
+
+const rockPaperScissors = (pl1, pl2) => {
+    console.log(`Computer chose ${computerMove}`);
+    console.log(`User chose ${usersMove}`)
+    if(computerMove === choice[0] && usersMove === choice[1]){
+        console.log(`User wins! ${choice[1]} covers ${choice[0]}`);
+    } else if(computerMove === choice[1] && usersMove === choice[0]){
+        console.log(`Computer wins! ${choice[1]} covers ${choice[0]}`);
+    } else if(computerMove === choice[0] && usersMove === choice[2]){
+        console.log(`Computer wins! ${choice[0]} crushes ${choice[2]}`)
+    } else if(computerMove === choice[2] && usersMove === choice [0]){
+        console.log(`User wins! ${choice[0]} crushes ${choice[2]}`);
+    } else if(computerMove === choice[0] && usersMove === choice[3]){
+        console.log(`Computer wins! ${choice[0]} crushes ${choice[3]}`);
+    } else if(computerMove === choice[3] && usersMove === choice[0]){
+        console.log(`User wins! ${choice[0]} crushes ${choice[3]}`);
+    } else if(computerMove === choice[0] && usersMove === choice[4]){
+        console.log(`User wins! ${choice[4]} vaporizes ${choice[0]}`);
+    } else if(computerMove === choice[4] && usersMove === choice[0]){
+        console.log(`Computer wins! ${choice[4]} vaporizes ${choice[0]}`);
+    } else if(computerMove === choice[1] && usersMove === choice[2]){
+        console.log(`User wins! ${choice[2]} cuts ${choice[1]}`);
+    } else if(computerMove === choice[2] && usersMove === choice[1]){
+        console.log(`Computer wins! ${choice[2]} cuts ${choice[1]}`);
+    } else if(computerMove === choice[1] && usersMove === choice[3]){
+        console.log(`User wins! ${choice[3]} eats ${choice[1]}`);
+    } else if(computerMove === choice[3] && usersMove === choice[1]){
+        console.log(`Computer wins! ${choice[3]} eats ${choice[1]}`);
+    } else if(computerMove === choice[1] && usersMove === choice[4]){
+        console.log(`Computer wins! ${choice[1]} disproves ${choice[4]}`);
+    } else if(computerMove === choice[4] && usersMove === choice[1]){
+        console.log(`User wins! ${choice[1]} disproves ${choice[4]}`);
+    } else if(computerMove === choice[2] && usersMove === choice[3]){
+        console.log(`Computer wins! ${choice[2]} decapitates ${choice[3]}`);
+    } else if(computerMove === choice[3] && usersMove === choice[2]){
+        console.log(`User wins! ${choice[2]} decapitates ${choice[3]}`);
+    } else if(computerMove === choice[2] && usersMove === choice[4]){
+        console.log(`User wins! ${choice[4]} smashes ${choice[2]}`);
+    } else if(computerMove === choice[4] && usersMove === choice[2]){
+        console.log(`Computer wins! ${choice[4]} smashes ${choice[2]}`);
+    } else if(computerMove === choice[3] && usersMove === choice[4]){
+        console.log(`Computer wins! ${choice[3]} poisons ${choice[4]}`);
+    } else if(computerMove === choice[4] && usersMove === choice[3]){
+        console.log(`User wins! ${choice[3]} poisons ${choice[4]}`);
+    } else {
+        console.log(`It's a tie. Try again.`);
+    }
+}
+
+rockPaperScissors(computerMove, usersMove)
